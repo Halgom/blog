402ed106b0a16c55b957f123fb891779f0853100
Halgom <fredv35@gmail.com>
2022-07-13 13:16:16 +0200

    Section 5 - Working with databases - Done

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,15 @@
 <x-layout>
     <x-slot name="content">
         <article>
-            <h1>{!! $post->title !!}</h1>
+            <h3>{{ $post->title }}</h3>
+
+            <p>
+                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </p>
+
             <div>
                 <p>
-                    {!! $post->body !!}
+                    {{ $post->body }}
                 </p>
             </div>
         </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,22 @@
 <x-layout>
     <x-slot name="content">
+
+        <div>
+            <h1>My Blog</h1>
+        </div>
+
         @foreach ($posts as $post)
             <article>
-                <h1>
+                <h3>
                     <a href="/posts/{{ $post->slug }}">
                         {{ $post->title }}
                     </a>
-                </h1>
+                </h3>
+
+                <p>
+                    By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+                </p>
+
                 <div>
                     <p>{{ $post->excerpt }}</p>
                 </div>
